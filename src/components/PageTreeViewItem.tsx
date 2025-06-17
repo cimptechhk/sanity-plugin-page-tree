d2f450bba6f2b03e7da8f659984e35145763e98b
CIMP Tech <info@cimptech.com>
2025-06-18 03:06:22 +0800

    Update PageTreeViewItem.tsx

--- src/components/PageTreeViewItem.tsx
+++ src/components/PageTreeViewItem.tsx
@@ -112,9 +112,9 @@ export const PageTreeViewItem = ({
             onMouseLeave={() => setIsHovered(false)}
             onClick={onItemClick}>
             <Flex align="center" gap={3}>
-              <Flex gap={1} direction="column">
-                <Text className="text-sm font-medium">{page.title}</Text>
-                <UrlText className="text-xs" isDisabled={isDisabled || (!page.isPublished && page.isDraft)} textOverflow="ellipsis">
+              <Flex gap={2} direction="column" className="p-2">
+                <Text className="text-sm font-medium text-[#252837]">{page.title}</Text>
+                <UrlText className="text-[0.8125rem] text-[#515870]" isDisabled={isDisabled || (!page.isPublished && page.isDraft)} textOverflow="ellipsis">
                   {parentPath ? (page.slug?.current ?? 'untitled') : (getRootPageSlug(page, config) ?? '/')}
                 </UrlText>
               </Flex>
